5a08f6556ceae10542bdde54a1edcecc09b8b4fd
kavyasara <77233322+kavyasara@users.noreply.github.com>
2021-02-21 23:35:35 +0530

    Create.C
    this file print the swapping of numbers

---
+++ .C
@@ -0,0 +1,11 @@
+int main() 
+{ 
+    int x, y; 
+    printf("Enter Value of x "); 
+    scanf("%d", &x); 
+    printf("\nEnter Value of y "); 
+    scanf("%d", &y); 
+    swap(x, y); 
+    printf("\nAfter Swapping: x = %d, y = %d", x, y); 
+    return 0; 
+} 
